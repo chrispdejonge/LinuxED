58d6e5d8d753b043f395c50f0acb676c6251d624
chrispdejonge <45596056+chrispdejonge@users.noreply.github.com>
2020-07-02 10:23:53 +0200

    Add files via upload

--- minion_toevoegen_nagios.sh
+++ minion_toevoegen_nagios.sh
@@ -4,7 +4,6 @@ read -p "Naam Minion?: " remote_host
 read -p "Ip address Minion?: " remote_host_ip
 cd "/usr/local/nagios/etc/servers/"
 sudo bash -c "cat << EOF > /usr/local/nagios/etc/servers/'$remote_host.cfg'
-
 #Ubuntu Host configuration file
 define host {
         use                          linux-server
@@ -12,7 +11,6 @@ define host {
         alias                        $remote_host
         address                      $remote_host_ip
         register                     1
-}
 }
 define service {
       host_name                       $remote_host
