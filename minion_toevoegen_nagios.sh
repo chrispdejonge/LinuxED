ee60b052e1b2bd8c9e7e7b3e2823cf3a4607d9c9
chrispdejonge <45596056+chrispdejonge@users.noreply.github.com>
2020-07-02 09:18:49 +0200

    Add files via upload

--- minion_toevoegen_nagios.sh
+++ minion_toevoegen_nagios.sh
@@ -13,41 +13,81 @@ define host {
         address                      $remote_host_ip
         register                     1
 }
-define service{                                                
-            use                     local-service           
-            host_name               ubuntu-nagios-client          
-            service_description     SWAP Uasge           
-            check_command           check_nrpe!check_swap                          
-                               
-}                                   
-                                  
-define service{                                                
-            use                     local-service           
-            host_name               ubuntu-nagios-client           
-            service_description     Root / Partition           
-            check_command           check_nrpe!check_root                         
-                                
-}                                 
-                               
-define service{            
-            use                     local-service           
-            host_name               ubuntu-nagios-client           
-            service_description     Current Users           
-            check_command           check_nrpe!check_users                                       
-}                                  
-                             
-define service{                                                    
-            use                     local-service           
-            host_name               ubuntu-nagios-client          
-            service_description     Total Processes           
-            check_command           check_nrpe!check_total_procs                              
-}                                  
-                                
-define service{              
-            use                     local-service           
-            host_name               ubuntu-nagios-client           
-            service_description     Current Load           
-            check_command           check_nrpe!check_load
+}
+define service {
+      host_name                       $remote_host
+      service_description             PING
+      check_command                   check_ping!100.0,20%!500.0,60%
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+define service {
+      host_name                       $remote_host
+      service_description             Check Users
+      check_command           check_local_users!20!50
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+define service {
+      host_name                       $remote_host
+      service_description             Local Disk
+      check_command                   check_local_disk!20%!10%!/
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+define service {
+      host_name                       $remote_host
+      service_description             Check SSH
+      check_command                   check_ssh
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+define service {
+      host_name                       $remote_host
+      service_description             Total Process
+      check_command                   check_local_procs!250!400!RSZDT
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
 }
 EOF"
 
